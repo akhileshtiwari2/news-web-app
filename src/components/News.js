f7769bd4f5e8c0d2f06a5b5c4717349a2920e6f6
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-06-22 14:06:35 +0530

    Loading spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -14,11 +15,13 @@ export class News extends Component {
   async componentDidMount() {
     let url =
       `https://newsapi.org/v2/top-headlines?country=in&apiKey=371f2c9b777744498051e549f499fdd4&page=1&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
+      this.setState({loading:true});
+      let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false
     });
   }
   handlePrevClick = async () => {
@@ -26,35 +29,40 @@ export class News extends Component {
     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=371f2c9b777744498051e549f499fdd4&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading: false
     });
   };
   handleNextClick = async () => {
     console.log("Next");
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)) {
-    } else {
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
+   
       let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=371f2c9b777744498051e549f499fdd4&page=${
         this.state.page + 1
       }&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading: false
       });
     }
   };
   render() {
     return (
       <div className="container my-3">
-        <h1>News- Top Headline</h1>
+        <h1 className="text-center">News- Top Headline</h1>
+        { this.state.loading && <Spinner/>}
 
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
