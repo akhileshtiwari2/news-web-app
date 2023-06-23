78fe9fe5fe7d3db0048898699d9052f84b3081e7
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-06-23 21:11:46 +0530

    Refactoring Code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import News from "./components/News";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize = 5;
   render() {
     return (
       <div>
@@ -18,7 +19,7 @@ export default class App extends Component {
               element={
                 <News
                   key="general"
-                  pageSize={6}
+                  pageSize={this.pageSize}
                   country="in"
                   category="general"
                 />
@@ -30,7 +31,7 @@ export default class App extends Component {
               element={
                 <News
                   key="business"
-                  pageSize={6}
+                  pageSize={this.pageSize}
                   country="in"
                   category="business"
                 />
@@ -42,7 +43,7 @@ export default class App extends Component {
               element={
                 <News
                   key="entertainment"
-                  pageSize={6}
+                  pageSize={this.pageSize}
                   country="in"
                   category="entertainment"
                 />
@@ -55,7 +56,7 @@ export default class App extends Component {
               element={
                 <News
                   key="health"
-                  pageSize={6}
+                  pageSize={this.pageSize}
                   country="in"
                   category="health"
                 />
@@ -67,7 +68,7 @@ export default class App extends Component {
               element={
                 <News
                   key="science"
-                  pageSize={6}
+                  pageSize={this.pageSize}
                   country="in"
                   category="science"
                 />
@@ -79,7 +80,7 @@ export default class App extends Component {
               element={
                 <News
                   key="sports"
-                  pageSize={6}
+                  pageSize={this.pageSize}
                   country="in"
                   category="sports"
                 />
@@ -91,7 +92,7 @@ export default class App extends Component {
               element={
                 <News
                   key="technology"
-                  pageSize={6}
+                  pageSize={this.pageSize}
                   country="in"
                   category="technology"
                 />
--- src/components/News.js
+++ src/components/News.js
@@ -23,8 +23,9 @@ export class News extends Component {
       page: 1,
     };
   }
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=371f2c9b777744498051e549f499fdd4&page=1&pageSize=${this.props.pageSize}`;
+
+  async updateNews(){
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=371f2c9b777744498051e549f499fdd4&page=${this.props.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -33,49 +34,67 @@ export class News extends Component {
       totalResults: parsedData.totalResults,
       loading: false,
     });
+
+  }
+
+  async componentDidMount() {
+    this.updateNews();
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=371f2c9b777744498051e549f499fdd4&page=1&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true });
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // this.setState({
+    //   articles: parsedData.articles,
+    //   totalResults: parsedData.totalResults,
+    //   loading: false,
+    // });
   }
   handlePrevClick = async () => {
     console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=${
-      this.props.country
-    }&category=${
-      this.props.category
-    }&apiKey=371f2c9b777744498051e549f499fdd4&page=${
-      this.state.page - 1
-    }&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false,
-    });
+    // let url = `https://newsapi.org/v2/top-headlines?country=${
+    //   this.props.country
+    // }&category=${
+    //   this.props.category
+    // }&apiKey=371f2c9b777744498051e549f499fdd4&page=${
+    //   this.state.page - 1
+    // }&pageSize=${this.props.pageSize}`;
+    // this.setState({ loading: true });
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // this.setState({
+    //   page: this.state.page - 1,
+    //   articles: parsedData.articles,
+    //   loading: false,
+    // });
+    this.setState({page:this.state.page - 1});
+    this.updateNews();
   };
   handleNextClick = async () => {
     console.log("Next");
-    if (
-      !(
-        this.state.page + 1 >
-        Math.ceil(this.state.totalResults / this.props.pageSize)
-      )
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${
-        this.props.country
-      }&category=${
-        this.props.category
-      }&apiKey=371f2c9b777744498051e549f499fdd4&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles,
-        loading: false,
-      });
-    }
+    // if (
+    //   !(
+    //     this.state.page + 1 >
+    //     Math.ceil(this.state.totalResults / this.props.pageSize)
+    //   )
+    // ) {
+    //   let url = `https://newsapi.org/v2/top-headlines?country=${
+    //     this.props.country
+    //   }&category=${
+    //     this.props.category
+    //   }&apiKey=371f2c9b777744498051e549f499fdd4&page=${
+    //     this.state.page + 1
+    //   }&pageSize=${this.props.pageSize}`;
+    //   this.setState({ loading: true });
+    //   let data = await fetch(url);
+    //   let parsedData = await data.json();
+    //   this.setState({
+    //     page: this.state.page + 1,
+    //     articles: parsedData.articles,
+    //     loading: false,
+    //   });
+    // }
+    this.setState({page:this.state.page + 1});
+    this.updateNews();
   };
   render() {
     return (
